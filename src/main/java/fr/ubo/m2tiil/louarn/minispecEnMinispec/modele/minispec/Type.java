f71f40bd60fb9a6245705ea218cb0ce442bdcef8
Robin Louarn <rlouarn@MacBook-Pro-de-Robin-2.local>
2019-03-14 14:14:14 +0100

    update

--- src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/modele/minispec/Type.java
+++
@@ -1,6 +0,0 @@
-package fr.ubo.m2tiil.louarn.minispecEnMinispec.modele.minispec;
-public class Type {
-	public Type() {
-		super();
-	}
-}
--- src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/repository/instance/AbstractInstance.java
+++
@@ -1,7 +0,0 @@
-package fr.ubo.m2tiil.louarn.minispecEnMinispec.repository.instance;
-
-import fr.ubo.m2tiil.louarn.minispecEnMinispec.repository.visiteurs.VisitableInstance;
-
-public abstract class AbstractInstance implements VisitableInstance {
-    public String id;
-}

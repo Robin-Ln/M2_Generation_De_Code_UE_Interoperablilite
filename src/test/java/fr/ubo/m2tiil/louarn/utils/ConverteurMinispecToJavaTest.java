d4c11d107fb79441147a5ffe737d4bf955312023
Robin Louarn <rlouarn@MacBook-Pro-de-Robin-2.local>
2019-03-14 21:47:56 +0100

    Update ConverteurMinispecToJavaTest.java

--- src/test/java/fr/ubo/m2tiil/louarn/utils/ConverteurMinispecToJavaTest.java
+++ src/test/java/fr/ubo/m2tiil/louarn/utils/ConverteurMinispecToJavaTest.java
@@ -1,8 +1,16 @@
 package fr.ubo.m2tiil.louarn.utils;
 
+import fr.ubo.m2tiil.louarn.modele.commun.Collection;
+import fr.ubo.m2tiil.louarn.modele.commun.TypeElement;
+import fr.ubo.m2tiil.louarn.modele.java.AttributeJava;
+import fr.ubo.m2tiil.louarn.modele.java.Clazz;
 import fr.ubo.m2tiil.louarn.modele.java.ModeleJava;
+import fr.ubo.m2tiil.louarn.modele.minispec.AttributeMinispec;
+import fr.ubo.m2tiil.louarn.modele.minispec.Entity;
 import fr.ubo.m2tiil.louarn.modele.minispec.ModeleMinispec;
+import fr.ubo.m2tiil.louarn.xml.ParserXmlDependance;
 import fr.ubo.m2tiil.louarn.xml.ParserXmlMinispec;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
@@ -22,11 +30,14 @@ class ConverteurMinispecToJavaTest {
     private DocumentBuilder builder;
     private Document document;
 
-    private ParserXmlMinispec parser;
+    private ParserXmlMinispec parserXmlMinispec;
 
     private ModeleMinispec modeleMinispec;
 
     private ModeleJava modeleJava;
+
+    private ConverteurMinispecToJava converteur;
+    private ParserXmlDependance parserDependance;
 
     @BeforeEach
     void init() throws ParserConfigurationException, IOException, SAXException {
@@ -42,20 +53,67 @@ class ConverteurMinispecToJavaTest {
         this.document = builder.parse(file);
 
         /**
-         * Initialisation du parser
+         * Initialisation du parserXmlMinispec
          */
-        this.parser = new ParserXmlMinispec(this.document);
-        this.parser.lire();
-        this.modeleMinispec = this.parser.getModeleMinispec();
+        this.parserXmlMinispec = new ParserXmlMinispec(this.document);
+        this.parserXmlMinispec.lire();
+        this.modeleMinispec = this.parserXmlMinispec.getModeleMinispec();
 
         /**
          * Initialiation du modele java
          */
-        // TODO j'ai besoin de tester le ârser de dependence
-//        ConverteurMinispecToJava converteu = new ConverteurMinispecToJava()
+        file = new File(getClass().getClassLoader().getResource(Constants.PATH_DEPENDANCES_XML).getPath());
+        if (!file.exists()) {
+            fail("La resource " + Constants.PATH_DEPENDANCES_XML + " n'est pas disponible");
+        }
+        this.document = builder.parse(file);
+        this.parserDependance = new ParserXmlDependance(this.document);
+        this.parserDependance.lire();
+        this.converteur = new ConverteurMinispecToJava(this.parserDependance.getDependencies());
+        this.modeleJava = converteur.convert(this.modeleMinispec);
     }
 
     @Test
-    void convert() {
+    void modeleTest() {
+        /**
+         * Test général de l'application
+         */
+        assertEquals(this.modeleJava.getName(), "modele");
+        assertEquals(this.modeleJava.getClazzes().size(), 2);
+    }
+
+    @Test
+    void entiteTest() {
+        /**
+         * Test de l'entite satelite
+         */
+        Clazz satellite = this.modeleJava.getClazzes().get(0);
+        assertEquals(satellite.getName(), "Satellite");
+        assertTrue(StringUtils.isBlank(satellite.getSupertype()));
+        assertEquals(satellite.getAttributeJavas().size(), 2);
+    }
+
+    @Test
+    void associationSimpleTest() {
+        /**
+         * Test de l'attribut nom de l'entite satellite
+         */
+        Clazz satellite = this.modeleJava.getClazzes().get(0);
+        AttributeJava nom = satellite.getAttributeJavas().get(0);
+        assertEquals(nom.getName(), "nom");
+        assertEquals(nom.getType(), new TypeElement("String"));
+    }
+
+    @Test
+    void associationMultipleTest() {
+
+        /**
+         * Test de l'attribut satellite de l'entite Flote
+         */
+        Clazz flote = this.modeleJava.getClazzes().get(1);
+        AttributeJava satelites = flote.getAttributeJavas().get(0);
+        assertEquals(satelites.getName(), "satellites");
+        assertEquals(satelites.getType(), new Collection("List", new TypeElement("Satellite")));
+
     }
 }

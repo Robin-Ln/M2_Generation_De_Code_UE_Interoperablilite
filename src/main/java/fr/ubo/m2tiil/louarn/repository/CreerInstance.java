7db0e67725ca6d3fe0822ee65b1835ae7027630b
Robin Louarn <rlouarn@MacBook-Pro-de-Robin-2.local>
2019-03-14 20:53:06 +0100

    commantaire

--- src/main/java/fr/ubo/m2tiil/louarn/repository/CreerInstance.java
+++ src/main/java/fr/ubo/m2tiil/louarn/repository/CreerInstance.java
@@ -1,83 +1,75 @@
 package fr.ubo.m2tiil.louarn.repository;
 
-import fr.ubo.m2tiil.louarn.modele.commun.TypeElement;
-import fr.ubo.m2tiil.louarn.modele.dependance.ReferenceModele;
-import fr.ubo.m2tiil.louarn.modele.java.Clazz;
-import fr.ubo.m2tiil.louarn.modele.java.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class CreerInstance {
 
-    private static final String P_NAME = "fr.ubo.m2tiil.louarn.minispecEnMinispec.repository.instance";
-
-
-    /**
-     * Constructeurs
-     */
-    public CreerInstance() {
-        super();
-    }
-
-    /**
-     * Methodes
-     */
-
-
-    private Clazz creerInstanceClass(Clazz aClazz) {
-
-        Clazz instance = new Clazz();
-        instance.setVisibilite(aClazz.getVisibilite());
-        instance.setPrototype(aClazz.getPrototype());
-        instance.setName(aClazz.getName() + "Instance");
-
-        instance.setSupertype("AbstractInstance");
-
-        instance.getDependances().add(new ReferenceModele("AbstractInstance", CreerRepository.P_NAME + "." + "AbstractInstance"));
-
-        for (AttributeJava attributeJava : aClazz.getAttributeJavas()) {
-            instance.getAttributeJavas().add(this.getAttribute(attributeJava.getName()));
-        }
-        return instance;
-    }
-
-
-
-    public ModeleJava creerInstanceModele(ModeleJava modeleJava) {
-
-        ModeleJava modeleJavaInstance = new ModeleJava();
-        modeleJavaInstance.setName(CreerInstance.P_NAME);
-
-        List<Clazz> instances = new ArrayList<>();
-
-        for (Clazz aClazz : modeleJava.getClazzes()) {
-            Clazz instance = this.creerInstanceClass(aClazz);
-            instance.setaPackage(CreerInstance.P_NAME);
-            instances.add(instance);
-        }
-
-        modeleJavaInstance.setClazzes(instances);
-        return modeleJavaInstance;
-    }
-
-
-    /**
-     * Methode utilile
-     */
-
-    AttributeJava getAttribute(String name) {
-        AttributeJava attributeJava = new AttributeJava();
-        attributeJava.setName(name);
-        attributeJava.setVisibilite(Visibilite.PUBLIC);
-
-        TypeElement typeElement = new TypeElement();
-        typeElement.setType("String");
-
-
-        attributeJava.setType(typeElement);
-
-
-        return attributeJava;
-    }
+//    private static final String P_NAME = "fr.ubo.m2tiil.louarn.minispecEnMinispec.repository.instance";
+//
+//
+//    /**
+//     * Constructeurs
+//     */
+//    public CreerInstance() {
+//        super();
+//    }
+//
+//    /**
+//     * Methodes
+//     */
+//
+//
+//    private Clazz creerInstanceClass(Clazz aClazz) {
+//
+//        Clazz instance = new Clazz();
+//        instance.setVisibilite(aClazz.getVisibilite());
+//        instance.setPrototype(aClazz.getPrototype());
+//        instance.setName(aClazz.getName() + "Instance");
+//
+//        instance.setSupertype("AbstractInstance");
+//
+//        instance.getDependances().add(new ReferenceModele("AbstractInstance", CreerRepository.P_NAME + "." + "AbstractInstance"));
+//
+//        for (AttributeJava attributeJava : aClazz.getAttributeJavas()) {
+//            instance.getAttributeJavas().add(this.getAttribute(attributeJava.getName()));
+//        }
+//        return instance;
+//    }
+//
+//
+//
+//    public ModeleJava creerInstanceModele(ModeleJava modeleJava) {
+//
+//        ModeleJava modeleJavaInstance = new ModeleJava();
+//        modeleJavaInstance.setName(CreerInstance.P_NAME);
+//
+//        List<Clazz> instances = new ArrayList<>();
+//
+//        for (Clazz aClazz : modeleJava.getClazzes()) {
+//            Clazz instance = this.creerInstanceClass(aClazz);
+//            instance.setaPackage(CreerInstance.P_NAME);
+//            instances.add(instance);
+//        }
+//
+//        modeleJavaInstance.setClazzes(instances);
+//        return modeleJavaInstance;
+//    }
+//
+//
+//    /**
+//     * Methode utilile
+//     */
+//
+//    AttributeJava getAttribute(String name) {
+//        AttributeJava attributeJava = new AttributeJava();
+//        attributeJava.setName(name);
+//        attributeJava.setVisibilite(Visibilite.PUBLIC);
+//
+//        TypeElement typeElement = new TypeElement();
+//        typeElement.setType("String");
+//
+//
+//        attributeJava.setType(typeElement);
+//
+//
+//        return attributeJava;
+//    }
 }
--- src/main/java/fr/ubo/m2tiil/louarn/tests/MinispecToJavaXml.java
+++ src/main/java/fr/ubo/m2tiil/louarn/tests/MinispecToJavaXml.java
@@ -1,125 +1,107 @@
 package fr.ubo.m2tiil.louarn.tests;
 
-import fr.ubo.m2tiil.louarn.utils.CheckHeritage;
-import fr.ubo.m2tiil.louarn.utils.ConverteurMinispecToJava;
-import fr.ubo.m2tiil.louarn.modele.java.Dependance;
-import fr.ubo.m2tiil.louarn.modele.java.ModeleJava;
-import fr.ubo.m2tiil.louarn.modele.minispec.ModeleMinispec;
-import fr.ubo.m2tiil.louarn.visiteurs.dependance.VisitorDependance;
-import fr.ubo.m2tiil.louarn.visiteurs.java.VisitorJavaPrinter;
-import fr.ubo.m2tiil.louarn.repository.CreerRepository;
-import fr.ubo.m2tiil.louarn.repository.CreerInstance;
-import fr.ubo.m2tiil.louarn.xml.ParserXmlDependance;
-import fr.ubo.m2tiil.louarn.xml.ParserXmlMinispec;
-import fr.ubo.m2tiil.louarn.xml.XmlErrorHandler;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class MinispecToJavaXml {
 
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setValidating(true);
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        XmlErrorHandler xmlErrorHandler = new XmlErrorHandler();
-        builder.setErrorHandler(xmlErrorHandler);
-
-        File xmlMinispec = new File("src/main/resources/XMLs/minispecXml/minispecEnMinispec.xml");
-        File xmlSatelite = new File("src/main/resources/XMLs/minispecXml/figure10.xml");
-        File xmlDependence = new File("src/main/resources/XMLs/java-code/figure16.xml");
-
-        Document document = builder.parse(xmlMinispec);
-
-
-        /*
-         * xmlMinispec
-         * Validation de la DTD
-         */
-        ModeleMinispec modeleMinispec = null;
-        ModeleMinispec modeleMinispecSatelite = null;
-        ModeleJava modeleJava = null;
-        ModeleJava modeleJavaSatelite = null;
-        if (xmlErrorHandler.isValide()) {
-            ParserXmlMinispec parserXml = new ParserXmlMinispec(document);
-            parserXml.lire();
-            modeleMinispec = parserXml.getModeleMinispec();
-            modeleJava = new ConverteurMinispecToJava(modeleMinispec).getModeleJava();
-
-
-            document = builder.parse(xmlSatelite);
-            parserXml = new ParserXmlMinispec(document);
-            parserXml.lire();
-            modeleMinispecSatelite = parserXml.getModeleMinispec();
-            modeleJavaSatelite = new ConverteurMinispecToJava(modeleMinispecSatelite).getModeleJava();
-        }
-
-        document = builder.parse(xmlDependence);
-
-        /*
-         * xmlDependence
-         * Validation de la DTD
-         */
-        List<Dependance> dependances = null;
-        document = builder.parse(xmlDependence);
-        if (xmlErrorHandler.isValide()) {
-            ParserXmlDependance parserXmlDependance = new ParserXmlDependance(document);
-            parserXmlDependance.lire();
-
-            dependances = parserXmlDependance.getDependencies();
-
-        }
-
-        if (!xmlErrorHandler.isValide()) {
-            for (SAXParseException saxParseException : xmlErrorHandler.getSAXParseExceptions()) {
-                System.out.println("Line : " + saxParseException.getLineNumber() + ", colonne : "
-                        + saxParseException.getColumnNumber() + " -----> " + saxParseException.getMessage());
-            }
-        }
-
-        CheckHeritage checkHeritage = new CheckHeritage();
-        checkHeritage.checkHeritage(modeleJava);
-        if (!checkHeritage.getExceptions().isEmpty()){
-            System.out.println("heritage KO");
-        }
-
-        VisitorDependance visitorDependance = new VisitorDependance(dependances);
-        visitorDependance.visite(modeleJava);
-        visitorDependance.visite(modeleJavaSatelite);
-
-        String path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/modele/minispec";
-        VisitorJavaPrinter visitorJavaPrinter = new VisitorJavaPrinter(path);
-        modeleJava.accept(visitorJavaPrinter);
-
-
-
-        path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/modele/autre";
-        visitorJavaPrinter = new VisitorJavaPrinter(path);
-        modeleJavaSatelite.accept(visitorJavaPrinter);
-
-
-        CreerInstance creerInstance = new CreerInstance();
-        ModeleJava modeleJavaSateliteInstance = creerInstance.creerInstanceModele(modeleJavaSatelite);
-
-        path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/repository/instance";
-        visitorJavaPrinter = new VisitorJavaPrinter(path);
-        modeleJavaSateliteInstance.accept(visitorJavaPrinter);
-
-        CreerRepository creerRepository = new CreerRepository();
-        ModeleJava modeleJavaRepesitory = creerRepository.creerRepositoryModele(modeleJavaSateliteInstance);
-
-
-        path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/repository/repository";
-        visitorJavaPrinter = new VisitorJavaPrinter(path);
-        modeleJavaRepesitory.accept(visitorJavaPrinter);
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        factory.setValidating(true);
+//        DocumentBuilder builder = factory.newDocumentBuilder();
+//        XmlErrorHandler xmlErrorHandler = new XmlErrorHandler();
+//        builder.setErrorHandler(xmlErrorHandler);
+//
+//        File xmlMinispec = new File("src/main/resources/XMLs/minispecXml/minispecEnMinispec.xml");
+//        File xmlSatelite = new File("src/main/resources/XMLs/minispecXml/figure10.xml");
+//        File xmlDependence = new File("src/main/resources/XMLs/java-code/figure16.xml");
+//
+//        Document document = builder.parse(xmlMinispec);
+//
+//
+//        /*
+//         * xmlMinispec
+//         * Validation de la DTD
+//         */
+//        ModeleMinispec modeleMinispec = null;
+//        ModeleMinispec modeleMinispecSatelite = null;
+//        ModeleJava modeleJava = null;
+//        ModeleJava modeleJavaSatelite = null;
+//        if (xmlErrorHandler.isValide()) {
+//            ParserXmlMinispec parserXml = new ParserXmlMinispec(document);
+//            parserXml.lire();
+//            modeleMinispec = parserXml.getModeleMinispec();
+//            modeleJava = new ConverteurMinispecToJava(modeleMinispec).getModeleJava();
+//
+//
+//            document = builder.parse(xmlSatelite);
+//            parserXml = new ParserXmlMinispec(document);
+//            parserXml.lire();
+//            modeleMinispecSatelite = parserXml.getModeleMinispec();
+//            modeleJavaSatelite = new ConverteurMinispecToJava(modeleMinispecSatelite).getModeleJava();
+//        }
+//
+//        document = builder.parse(xmlDependence);
+//
+//        /*
+//         * xmlDependence
+//         * Validation de la DTD
+//         */
+//        List<Dependance> dependances = null;
+//        document = builder.parse(xmlDependence);
+//        if (xmlErrorHandler.isValide()) {
+//            ParserXmlDependance parserXmlDependance = new ParserXmlDependance(document);
+//            parserXmlDependance.lire();
+//
+//            dependances = parserXmlDependance.getDependencies();
+//
+//        }
+//
+//        if (!xmlErrorHandler.isValide()) {
+//            for (SAXParseException saxParseException : xmlErrorHandler.getSAXParseExceptions()) {
+//                System.out.println("Line : " + saxParseException.getLineNumber() + ", colonne : "
+//                        + saxParseException.getColumnNumber() + " -----> " + saxParseException.getMessage());
+//            }
+//        }
+//
+//        CheckHeritage checkHeritage = new CheckHeritage();
+//        checkHeritage.checkHeritage(modeleJava);
+//        if (!checkHeritage.getExceptions().isEmpty()){
+//            System.out.println("heritage KO");
+//        }
+//
+//        VisitorDependance visitorDependance = new VisitorDependance(dependances);
+//        visitorDependance.visite(modeleJava);
+//        visitorDependance.visite(modeleJavaSatelite);
+//
+//        String path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/modele/minispec";
+//        VisitorJavaPrinter visitorJavaPrinter = new VisitorJavaPrinter(path);
+//        modeleJava.accept(visitorJavaPrinter);
+//
+//
+//
+//        path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/modele/autre";
+//        visitorJavaPrinter = new VisitorJavaPrinter(path);
+//        modeleJavaSatelite.accept(visitorJavaPrinter);
+//
+//
+//        CreerInstance creerInstance = new CreerInstance();
+//        ModeleJava modeleJavaSateliteInstance = creerInstance.creerInstanceModele(modeleJavaSatelite);
+//
+//        path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/repository/instance";
+//        visitorJavaPrinter = new VisitorJavaPrinter(path);
+//        modeleJavaSateliteInstance.accept(visitorJavaPrinter);
+//
+//        CreerRepository creerRepository = new CreerRepository();
+//        ModeleJava modeleJavaRepesitory = creerRepository.creerRepositoryModele(modeleJavaSateliteInstance);
+//
+//
+//        path = "src/main/java/fr/ubo/m2tiil/louarn/minispecEnMinispec/repository/repository";
+//        visitorJavaPrinter = new VisitorJavaPrinter(path);
+//        modeleJavaRepesitory.accept(visitorJavaPrinter);
 
     }
 }
